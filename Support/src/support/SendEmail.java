8b70244c64ccd7071c26e2de6eb3050cd9ca9981
Maru <maruanibrahim22@gmail.com>
2022-12-06 11:50:40 -0300

    Envio de email direto por java

---
+++ Support/src/support/SendEmail.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package support;
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ *
+ * @author 1m4ru
+ */
+public class SendEmail {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+             Properties props = new Properties();
+            /** Parâmetros de conexão com servidor Hotmail */
+            props.put("mail.transport.protocol", "smtp");
+            props.put("mail.smtp.host", "smtp.live.com");
+            props.put("mail.smtp.socketFactory.port", "587");
+            props.put("mail.smtp.socketFactory.fallback", "false");
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.port", "587");
+
+            Session session = Session.getDefaultInstance(props,
+                        new javax.mail.Authenticator() {
+                             protected PasswordAuthentication getPasswordAuthentication()
+                             {
+                                   return new PasswordAuthentication("imoussamaru@hotmail.com", "02101994");
+                             }
+                        });
+
+            /** Ativa Debug para sessão */
+            session.setDebug(true);
+
+            try {
+
+                  Message message = new MimeMessage(session);
+                  message.setFrom(new InternetAddress("imoussamaru@hotmail.com")); //Remetente
+
+                  message.setRecipients(Message.RecipientType.TO,
+                                    InternetAddress.parse("imoussamaru@hotmail.com")); //Destinatário(s)
+                  message.setSubject("Chamado fechado");//Assunto
+                  message.setText("Seu chamado 0000 foi resolvido com sucesso");
+                  /**Método para enviar a mensagem criada*/
+                  Transport.send(message);
+
+                  System.out.println("Feito!!!");
+
+             } catch (MessagingException e) {
+                  throw new RuntimeException(e);
+            }
+      }
+    
+    
+    
+}
